cf775c3f50f4ff69b9693fde3ac4fbacfefd379c
Lodestar-test <moya.pochhta@gmail.com>
2025-11-09 13:10:03 +0300

    Интеграция Pytest и Playwright-DZ

--- playwright_courses.py
+++ playwright_courses.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import sync_playwright, expect
 
-with sync_playwright() as playwrigh:
-    browser = playwrigh.chromium.launch(headless=False)
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
