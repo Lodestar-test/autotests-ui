dbe50e71221573295b7fd96d1d27ee77b73935b7
Lodestar-test <moya.pochhta@gmail.com>
2025-12-12 22:28:57 +0300

    Знакомство с PageObject

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,9 +5,9 @@ class LoginPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        self.password_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        self.login_button = page.get_by_test_id('registration-page-registration-button')
+        self.email_input = page.get_by_test_id('login-form-email-input').locator('input')
+        self.password_input = page.get_by_test_id('login-form-password-input').locator('input')
+        self.login_button = page.get_by_test_id('login-page-login-button')
         self.registration_link = page.get_by_test_id('login-page-registration-link')
         self.wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
 
